d13870908bfca509e89bbda6929cba03e53301d7
papp11gergely13 <90318540+papp11gergely13@users.noreply.github.com>
2024-01-17 19:26:26 +0000

    commiT

--- 1_✨_Homepage.py
+++ 1_✨_Homepage.py
@@ -1,17 +1,4 @@
 import streamlit as st 
 
-st.set_page_config(
-    page_title="Multipage App",
-    page_icon="📖",
-)
-st.title("Main Page")
-st.sidebar.success("Select a page above.")
-
-if "my_input" not in st.session_state:
-    st.session_state["my_input"] = ""
-
-my_input = st.text_input("Input a text here", st.session_state["my_input"])
-submit = st.button("Submit")
-if submit:
-    st.session_state["my_input"] = my_input
-    st.write("You have entered: ", my_input)
+st.title("Project")
+st.write("You have entered", st.session_state["my_input"])
